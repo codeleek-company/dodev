21a65d39a66a9b19547e948a1e64e2ceed13c2bf
devcompany-public <company.dev.public@gmail.com>
2023-09-24 18:10:53 +0300

    Edit in footer design

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,23 +3,25 @@ import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <div className="container p-3 bg-bar">
-      <div className="flex justify-evenly uppercase mt-2">
-        <Link to="/dashboard/projects">Projects</Link>
-        <Link to="/dashboard/ideas">Ideas</Link>
-        <Link to="/dashboard/resources">Resources</Link>
-        <Link to="/dashboard/roadmap">Roadmap</Link>
-      </div>
-      <div className="border-t-2 mt-5 pt-2 flex justify-between flex-wrap">
-        <span className="uppercase">
-          Copyrights <Copyright /> Osama Mohammed
-        </span>
-        {/* Links */}
-        <div className="flex gap-2">
-          <Twitter />
-          <Youtube />
-          <Facebook />
-          <Instagram />
+    <div className="bg-bar">
+      <div className="container p-3">
+        <div className="flex justify-evenly uppercase mt-2 text-[#aaa]">
+          <Link to="/dashboard/projects">Projects</Link>
+          <Link to="/dashboard/ideas">Ideas</Link>
+          <Link to="/dashboard/resources">Resources</Link>
+          <Link to="/dashboard/roadmap">Roadmap</Link>
+        </div>
+        <div className="border-t-2 mt-5 pt-2 flex justify-between flex-wrap">
+          <span className="uppercase dark:text-[#999] text-[#333] text-lg">
+            Copyrights <Copyright /> Osama Mohammed
+          </span>
+          {/* Links */}
+          <div className="flex gap-2">
+            <Twitter />
+            <Youtube />
+            <Facebook />
+            <Instagram />
+          </div>
         </div>
       </div>
     </div>
