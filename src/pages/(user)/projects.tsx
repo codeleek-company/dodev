365e72d1871060e207aa49bb3e77f84c3cf3f1e4
os-mhmd <docosmo010@gmail.com>
2024-06-03 18:14:24 +0300

    style/organizing

--- src/pages/(user)/projects.tsx
+++ src/pages/(user)/projects.tsx
@@ -1,24 +1,25 @@
-import State from "@/components/state";
 import title from "@/utils/title";
-import { Info, PenBox } from "lucide-react";
+import { Info } from "lucide-react";
 
 export default function Projects() {
   title("Projects");
 
   return (
-    <div className="mt-2 container mb-20">
+    <div className="mt-6 container mb-20">
       <section>
         <div className="bg-muted_diff rounded-t-md p-3 text-center">
           <Info />
         </div>
         <div className="bg-muted rounded-b-md p-3">
-          <State isolate color={2}>
-            project name
-          </State>
-          <span className="text-blue-200">A Name</span>
-          <PenBox width={20} className="ms-1" />
+          <div className="mb-2">
+            <span className="m-2">Project Name:</span>
+            <span className="text-blue-200">...</span>
+          </div>
+          <div className="mb-2">
+            <span className="m-2">Description:</span>
+            <span className="text-blue-200">...</span>
+          </div>
         </div>
-        <div></div>
       </section>
     </div>
   );
